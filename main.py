a742122e19753838229da455b0885828ee24abdd
AleksandrZhuravlev <mafanyazhura@gmail.com>
2020-03-16 21:49:02 +0700

    file reading and spliting

--- main.py
+++ main.py
@@ -2,3 +2,11 @@
 # Developers:   Drachev Nikita (),
 #               Starnovskiy Sergey (),
 #               Zhuravlev Alexander ()
+
+with open('input.txt','r') as infile:
+    initialtext=infile.read()
+    initialtext.split(' ')
+
+while initialtext.find('\n')!=-1:
+    initialtext.replace('\n',' ')
+print(initialtext)
